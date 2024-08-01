2c514c82b5b2fea42dbf02bc26c223b15e9f453f
Haylee Schäfer <haylee@inventivetalent.org>
2024-08-01 15:41:38 +0200

    rebuild

--- dist/generate/index.js
+++ dist/generate/index.js
@@ -15,4 +15,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./DelayInfo"), exports);
+__exportStar(require("./GenerateType"), exports);
 //# sourceMappingURL=index.js.map
