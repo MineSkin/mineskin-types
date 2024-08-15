9ed075a65ced7806fc9ed04d8a2a449fa24d225a
Haylee Schäfer <haylee@inventivetalent.org>
2024-08-15 12:11:37 +0200

    .

--- dist/skin/SkinTexture.d.ts
+++ dist/skin/SkinTexture.d.ts
@@ -2,4 +2,6 @@ import { SkinTextureUrls } from "./SkinTextureUrls";
 import { ProfileProperty } from "../profile";
 export interface SkinTexture extends Omit<ProfileProperty, "name"> {
     urls: SkinTextureUrls;
+    /**@deprecated**/
+    url: string;
 }
--- src/skin/SkinTexture.ts
+++ src/skin/SkinTexture.ts
@@ -3,4 +3,6 @@ import { ProfileProperty } from "../profile";
 
 export interface SkinTexture extends Omit<ProfileProperty, "name"> {
     urls: SkinTextureUrls;
+    /**@deprecated**/
+    url: string;
 }
