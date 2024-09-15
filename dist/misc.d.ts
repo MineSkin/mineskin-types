61e24acc9f7eefb20d6351560a3d2590cd0b752f
Haylee Schäfer <haylee@inventivetalent.org>
2024-09-15 15:40:45 +0200

    add breadcrumb

--- dist/misc.d.ts
+++ dist/misc.d.ts
@@ -1,2 +1,3 @@
 export type UUID = string;
 export type Maybe<T> = T | undefined;
+export type Breadcrumb = string;
--- src/misc.ts
+++ src/misc.ts
@@ -1,2 +1,4 @@
 export type UUID = string;
 export type Maybe<T> = T | undefined;
+
+export type Breadcrumb = string;
