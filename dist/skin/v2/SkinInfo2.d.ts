4f277712514bd4dd54090ae5c62c34382fca1477
Haylee Schäfer <haylee@inventivetalent.org>
2024-09-17 22:03:26 +0200

    ?

--- dist/skin/v2/SkinInfo2.d.ts
+++ dist/skin/v2/SkinInfo2.d.ts
@@ -1,13 +1,13 @@
 import { SkinTexture2 } from "./SkinTexture2";
 import { GeneratorInfo } from "./GeneratorInfo";
-import { SkinVisibility } from "../SkinVisibility";
 import { SkinVariant } from "../SkinVariant";
 import { UUID } from "../../misc";
+import { SkinVisibility2 } from "./SkinVisibility2";
 export interface SkinInfo2 {
     uuid: UUID;
     name?: string;
     variant: SkinVariant;
-    visibility: SkinVisibility;
+    visibility: SkinVisibility2;
     texture: SkinTexture2;
     generator: GeneratorInfo;
     views: number;
