a49d4b4efd171e5279ebdb1776e05ab73188610d
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-14 11:27:40 +0200

    add GenerateOptions

---
+++ dist/generate/GenerateOptions.d.ts
@@ -0,0 +1,6 @@
+import { SkinVariant, SkinVisibility2 } from "../skin";
+export interface GenerateOptions {
+    name?: string;
+    variant: SkinVariant;
+    visibility: SkinVisibility2;
+}
---
+++ src/generate/GenerateOptions.ts
@@ -0,0 +1,7 @@
+import { SkinVariant, SkinVisibility2 } from "../skin";
+
+export interface GenerateOptions{
+    name?: string;
+    variant: SkinVariant;
+    visibility: SkinVisibility2;
+}
