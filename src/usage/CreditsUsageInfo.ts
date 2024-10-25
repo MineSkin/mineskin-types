fa7ffb931b2ec734f3018ad118c98c7c5e9654a0
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-25 21:43:38 +0200

    update UsageInfo

--- src/usage/CreditsUsageInfo.ts
+++ src/usage/CreditsUsageInfo.ts
@@ -1,8 +1,7 @@
 import { CreditType } from "../billing";
 
 export interface CreditsUsageInfo {
-    id: string;
-    type: CreditType;
+    type?: CreditType;
     used: number;
     remaining: number;
 }
