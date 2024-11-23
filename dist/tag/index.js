b83b51a7f8b5331d01753c48e305448fbf4ebeb8
Haylee Schäfer <haylee@inventivetalent.org>
2024-11-23 13:17:43 +0100

    fix export

--- dist/tag/index.js
+++ dist/tag/index.js
@@ -15,4 +15,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./TagVoteType"), exports);
+__exportStar(require("./TagInfo"), exports);
 //# sourceMappingURL=index.js.map
